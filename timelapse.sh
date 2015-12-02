2549f954b76cc98b7c39fd8390558d634b7df8f7
Robby Cochran <rac@cs.unc.edu>
2015-12-02 15:29:27 -0500

    Added new features:
    - Capture multiple screens at once by default
    - Output to a timestamped directory
    - Add timestamp annotation to webcam catpure
    - rename isight to webcam

--- timelapse.sh
+++ timelapse.sh
@@ -6,19 +6,59 @@
 #
 # Can also record using webcam via the imagesnap program.
 #
+output_dir="output"
+output_dir="capture-$(date '+%Y-%m-%dT%H%M%S')"
+screen_size="1400x900!"
+SCREENS=( 2 1 3 )
+#SCREENS=( 1 )
+FFMPEG_BIN="ffmpeg -loglevel panic -y "
+
+mkdir -p $output_dir/screen $output_dir/webcam
+SCREEN_LEN=${#SCREENS[@]}
+for (( i=0; i<${SCREEN_LEN}; ++i)); do
+  mkdir -p $output_dir/screen${SCREENS[$i]}
+done
+
 while true; do
     timestamp=$(date '+%Y-%m-%dT%H%M%S');
-    echo "Capturing screen at $timestamp."
+    screen_file="${output_dir}/screen/screen-$timestamp.jpg"
+    screen_filter="${output_dir}/screen%d/screen-$timestamp.jpg"
+    webcam_file="${output_dir}/webcam/webcam-$timestamp.jpg"
+
     # For one screen:
-    screencapture -t jpg -x "output/screen/screen1-$timestamp.jpg";
-    # For two screens:
-    # screencapture -x "screen1-$timestamp.png" "screen2-$timestamp.png";
+    if [ $SCREEN_LEN -eq 1 ]; then
+      echo "Capturing screen at $timestamp."
+      screencapture -t jpg -x ${screen_file}
+    else
+      # For multiple screens:
+      echo "Capturing ${SCREEN_LEN} screens at $timestamp."
+      capture_files=""
+      for (( i=0; i<${SCREEN_LEN}; ++i)); do
+        capture_files+=" ${output_dir}/screen${SCREENS[$i]}/screen-${timestamp}.jpg "
+      done
+      screencapture -t jpg -x ${capture_files}
+
+      # Scale
+      for (( i=0; i<${SCREEN_LEN}; ++i)); do
+        file_name="${output_dir}/screen${SCREENS[$i]}/screen-${timestamp}.jpg "
+        convert ${file_name} -scale ${screen_size} ${file_name}
+      done
+
+      # Combine 
+      ${FFMPEG_BIN} -i ${screen_filter} -filter_complex tile=${SCREEN_LEN}x1 ${screen_file}
+    fi
+
 
     # If you have a webcam, you might want to capture that too.
-    echo "Capturing isight at $timestamp."
-    ./imagesnap/imagesnap -q "output/isight/isight-$timestamp.jpg";
-    # sleep 60;
-    sleep 120;
+    echo "Capturing webcam at $timestamp."
+    image_webcam="${output_dir}/webcam/webcam-$timestamp.jpg"
+    #./imagesnap/imagesnap -q "${output_dir}/webcam/webcam-$timestamp.jpg";
+    imagesnap -w 2 -q ${image_webcam}
+    annotation="$(date '+%m-%d-%Y %l:%M %p')"
+    pointsize=72
+    convert ${image_webcam} -font arial -pointsize $pointsize -fill white -gravity southeast -annotate +10+10 "${annotation}" ${image_webcam}
+
+    sleep 28
 done;
 
 # After it's finished, you probably want to play around with image
